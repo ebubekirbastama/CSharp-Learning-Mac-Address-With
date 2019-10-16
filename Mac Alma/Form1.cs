65cc29f23bd60b0a1ba21e07c92c169f218b4670
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2019-10-16 23:11:41 +0300

    Update Form1.cs

--- Mac Alma/Form1.cs
+++ Mac Alma/Form1.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System;
 using System.Management;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//Coding By& Siyah Muhafız...
 namespace Mac_Alma
 {
     public partial class Form1 : Form
@@ -17,7 +10,7 @@ namespace Mac_Alma
         {
             InitializeComponent();
         }
-
+        //Coding By& Siyah Muhafız...
         private void Form1_Load(object sender, EventArgs e)
         {
             textBox1.Text = Mac();
